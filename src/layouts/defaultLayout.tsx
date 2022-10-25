4134c8605ab22e0d30065a7940b2f969bed5220b
PATRICKOTERONOT\patri <patrick.rocha.otero@gmail.com>
2022-10-24 23:16:05 -0300

    Agora, ao clicar do lado de fora dos menus, o bot retorna para mostrar o menu inicial, e, apenas quando é clicado do lado de fora durante o menu inicial é que o bot retorna ao estado inicial, sem mostrar nenhum menu

--- src/layouts/defaultLayout.tsx
+++ src/layouts/defaultLayout.tsx
@@ -10,7 +10,7 @@ export function DefaultLayout() {
 
     const { earthPosition } = useBotFunctionsContext();
 
-    const {setHoloPosition, setHomePage, setBotPosition, setEarthPosition, setHologramActivated, setIsShowingMenu, setInfoTextHolo, setVisorPosition, setEyeState, setAboutMePage } = useBotFunctionsContext();
+    const {homePage, aboutMePage, portfolioPage , setPortfolioPage, setHoloPosition, setHomePage, setBotPosition, setEarthPosition, setHologramActivated, setIsShowingMenu, setInfoTextHolo, setVisorPosition, setEyeState, setAboutMePage } = useBotFunctionsContext();
 
     const navigate = useNavigate();
 
@@ -18,17 +18,35 @@ export function DefaultLayout() {
     return (
        <DefaultContainer>
             <div  className="layout-clicable-container" onClick={() => {
-                    setBotPosition("")
-                    setEarthPosition("")
-                    setAboutMePage(false)
-                    setHomePage(false)
-                    setHologramActivated(false);
-                    setIsShowingMenu(false);
-                    setInfoTextHolo(false);
-                    setVisorPosition("visor-to-top");
-                    setEyeState("")
-                    setHoloPosition("")
-                    navigate("/")}}>
+                    if (homePage) {
+                        setBotPosition("")
+                        setEarthPosition("")
+                        setAboutMePage(false)
+                        setHomePage(false)
+                        setHologramActivated(false);
+                        setIsShowingMenu(false);
+                        setInfoTextHolo(false);
+                        setVisorPosition("visor-to-top");
+                        setEyeState("")
+                        setHoloPosition("")
+                        navigate("/")
+                    }
+                    if (aboutMePage) {
+                        setAboutMePage(false);
+                        setHomePage(true)
+                        setBotPosition("bot-showing-menu");
+                        setEarthPosition("")
+                        navigate("/")
+                    }
+                    if (portfolioPage) {
+                        setPortfolioPage(false);
+                        setHomePage(true)
+                        setBotPosition("bot-showing-menu");
+                        setEarthPosition("")
+                        setHoloPosition("")
+                        navigate("/")
+                            }
+                    }}>
             </div>
                 <GreetBot/>
                 <StarsBackground/>
